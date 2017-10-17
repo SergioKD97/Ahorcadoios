2e2d2a57ad42ed1fe63e57358ad98a27ad086869
CETYS <cetys@cetyss-mac.local>
2017-10-17 23:48:08 +0200

    lk

--- ahorcado/ViewController.swift
+++ ahorcado/ViewController.swift
@@ -10,6 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var palabraOculta1 = "PASTAR" as NSString;
+    var letra : String = "";
+    
+    
+    @IBOutlet var palabraOculta: UILabel!
+    
+    
+    
+    
+    @IBAction func letraPulsada(_ sender: UIButton) {
+        
+        let letra = sender.currentTitle
+        let acierto = 0
+        let LabelPalabraOculta = palabraOculta.text! as NSString
+
+        
+        for i in 0...palabraOculta1.length-1{
+            let rango = NSMakeRange(i, 1)
+            let rango1 = NSMakeRange(1, 1)
+            
+            if(letra == palabraOculta1.substring(with: rango) ){
+                
+                
+                LabelPalabraOculta.replacingCharacters(in: rango1, with: "S")
+                
+                //print("rango1 : ",  rango1 )
+                
+                
+            }
+        }
+
+
+    }
+    
+    
+    
+    
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
